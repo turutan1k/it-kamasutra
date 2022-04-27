273fc35a9cec63cc63df68c3c6e27e060d07e7e6
turutan1k <turuta.nik1@gmail.com>
2022-04-27 23:35:08 +0300

    import export

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
 function App() {
     return (
         <div className="App">
             <Header />
             <h1>--------------</h1>
             <Technologies />
-        </div>
-    );
-}
-
-function Header() {
-    return (
-        <div className="App">
-            <a href="/#">Home</a>
-            <a href="/#">Message</a>
-            <a href="/#">News</a>
-            <a href="/#">Feed</a>
-        </div>
-    );
-}
-
-function Technologies() {
-    return (
-        <div className="App">
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
         </div>
     );
 }
---
+++ my-app/src/Technologies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Technologies() {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
