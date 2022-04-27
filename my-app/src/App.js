1fc6c0f2475dcd1727df5de7b791f7466fea5dff
turutan1k <turuta.nik1@gmail.com>
2022-04-27 22:41:30 +0300

    what component is?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return <div className='App'>1mplr html code</div>;
+    return (
+        <div className="App">
+            <Header />
+            <h1>--------------</h1>
+            <Technologies />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div className="App">
+            <a href="/#">Home</a>
+            <a href="/#">Message</a>
+            <a href="/#">News</a>
+            <a href="/#">Feed</a>
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
