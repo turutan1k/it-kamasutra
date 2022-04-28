b1d6663aa934300240b1d8213e340558f180cdf4
turutan1k <turuta.nik1@gmail.com>
2022-04-29 00:34:10 +0300

    breakdown into components, styling

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                alt="logo"
+                src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo-700x394.png"
+            />
+            <div>Social Network</div>
+            <div></div>
+        </header>
+    );
+};
+
+export default Header;
