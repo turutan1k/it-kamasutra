602555dc7459df62dd0671036bafb8b695e0cd1f
turutan1k <turuta.nik1@gmail.com>
2022-05-09 23:49:59 +0300

    dialogs content, exact, /*

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+
 export const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog}>Dymich</div>
-                <div className={classes.dialog}>Valera</div>
-                <div className={classes.dialog}>Andrey</div>
-                <div className={classes.dialog}>Nikita</div>
-                <div className={classes.dialog}>Vladimir</div>
-                <div className={classes.dialog}>Grigoriy</div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/1">Dymich</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/2">Valera</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/3">Misha</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4">Igor</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">Pavel</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/6">Andrey</NavLink>
+                </div>
             </div>
             <div className={classes.messages}>
                 <div className={classes.message}>Hello</div>
--- my-app/src/components/ProfileContent/MyPosts/MyPosts.jsx
+++ my-app/src/components/ProfileContent/MyPosts/MyPosts.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
-import { Button, Space } from 'antd';
+import { Button } from 'antd';
 
 export const MyPosts = () => {
     return (
         <>
             My posts
             <div>
-                <textarea></textarea>
-                <Space style={{ width: '100%' }}>
-                    <Button type="primary">Add post</Button>
-                </Space>
+                <textarea placeholder="Write a description below..."></textarea>
             </div>
+            <Button type="primary">Add post</Button>
             <div className={classes.posts}>
                 <Post message="HI, How are u?" likesCount="21" />
                 <Post message="It's my first post" likesCount="10130" />
