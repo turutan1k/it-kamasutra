2ec726c58d1c532eef575d7b51e822cef0ba0403
turutan1k <turuta.nik1@gmail.com>
2022-05-09 16:22:27 +0300

    develop Dialogs.jsx page

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
-// import classes from './Dialogs.module.css';
+import classes from './Dialogs.module.css';
 export const Dialogs = () => {
-    return <div>Dialogs</div>;
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>Dymich</div>
+                <div className={classes.dialog}>Valera</div>
+                <div className={classes.dialog}>Andrey</div>
+                <div className={classes.dialog}>Nikita</div>
+                <div className={classes.dialog}>Vladimir</div>
+                <div className={classes.dialog}>Grigoriy</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>Bless you!</div>
+            </div>
+        </div>
+    );
 };
