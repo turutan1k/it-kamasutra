deab447dba65575aaeae64d2a69b9c6888621f99
turutan1k <turuta.nik1@gmail.com>
2022-05-10 12:44:46 +0300

    add exact path for route /dialogs, using *

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,7 +20,7 @@ function App() {
                 <div className="app-wrapper-content">
                     <Routes>
                         <Route path="/profile" element={<ProfileContent />} />
-                        <Route path="/dialogs" element={<Dialogs />} />
+                        <Route path="/dialogs/*" element={<Dialogs />} />
                         <Route path="/news" element={<News />} />
                         <Route path="/music" element={<Music />} />
                         <Route path="/settings" element={<Settings />} />
