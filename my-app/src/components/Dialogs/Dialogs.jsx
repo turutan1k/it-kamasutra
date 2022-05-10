6c754f2a4a5bbd8e7064e995a60d33e5bf2b0f8d
turutan1k <turuta.nik1@gmail.com>
2022-05-10 15:27:02 +0300

    moved component Message to a separate file

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
-
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return (
-        <div className={styles.dialog}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    );
-};
-
-const Message = (props) => {
-    return <div className={styles.message}>{props.message}</div>;
-};
+import DialogItem from './DialogItem';
+import Message from './Message';
 
 export const Dialogs = () => {
     return (
---
+++ my-app/src/components/Dialogs/Message.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Message = (props) => {
+    return <div className={styles.message}>{props.message}</div>;
+};
+export default Message;
